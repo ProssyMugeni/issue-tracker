7d34babd5d2c821c11e299d2a87835fe772aa6cb
ProssyMugeni <mugeniprossy53@gmail.com>
2019-10-11 12:44:29 +0300

    issue tracker

---
+++ index.js
@@ -0,0 +1,29 @@
+// alert('wow')
+
+
+// for loop
+
+for(let i= 0; i < 5; i++){
+    console.log(i)
+}
+
+for(let x= 15; x>=10; x--){
+    console.log(x)
+}
+
+
+
+
+let txt ='javascript';
+let x
+
+for(x of txt){
+    console.log(x);
+}
+
+let car = ['ferari', 'markx', 'rangerover' ];
+let v;
+for(v of car){
+    console.log(v)
+}
+
---
+++ main.js
@@ -0,0 +1,91 @@
+function fetchIssues(){
+    let issues = JSON.parse(localStorage.getItem(issues));
+    let issuesList = document.getElementById('issuesList');
+    issuesList.innerHTML="";
+
+    for(let i = 0; i < issues.length; i++){
+        let id = issues[i].id;
+        let desc = issues[i].description;
+        let severity = issues[i].severity;
+        let status = issues[i].status;
+
+        issuesList.innerHTML +=   '<div class="well">'+
+                              '<h6>Issue ID: ' + id + '</h6>'+
+                              '<p><span class="label label-info">' + status + '</span></p>'+
+                              '<h3>' + desc + '</h3>'+
+                              '<p><span class="glyphicon glyphicon-time"></span> ' + severity + ' '+
+                              '<span class="glyphicon glyphicon-user"></span> ' + assignedTo + '</p>'+
+                              '<a href="#" class="btn btn-warning" onclick="setStatusClosed(\''+id+'\')">Close</a> '+
+                              '<a href="#" class="btn btn-danger" onclick="deleteIssue(\''+id+'\')">Delete</a>'+
+                              '</div>';
+    }
+}
+
+
+document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
+
+
+
+function saveIssue(e) {
+    let issueId = chance.guid();
+    let issueDesc = document.getElementById('issueDescInput').value;
+    let issueSeverity = document.getElementById('issueSeverityInput').value;
+    let issueAssignedTo = document.getElementById('issueAssignedToInput').value;
+    let issueStatus = 'Open';
+    let issue = {
+      id: issueId,
+      description: issueDesc,
+      severity: issueSeverity,
+      assignedTo: issueAssignedTo,
+      status: issueStatus
+    }
+    
+    if (localStorage.getItem('issues') === null) {
+      let issues = [];
+      issues.push(issue);
+      localStorage.setItem('issues', JSON.stringify(issues));
+    } else {
+      let issues = JSON.parse(localStorage.getItem('issues'));
+      issues.push(issue);
+      localStorage.setItem('issues', JSON.stringify(issues));
+    }
+    
+    document.getElementById('issueInputForm').reset();
+   
+    fetchIssues();
+    
+    e.preventDefault(); 
+  }
+  <a href="#" class="btn btn-warning" onclick="setStatusClosed(\''+id+'\')">Close</a>
+
+
+
+  function setStatusClosed (id) {
+    var issues = JSON.parse(localStorage.getItem('issues'));
+    
+    for(var i = 0; i < issues.length; i++) {
+      if (issues[i].id == id) {
+        issues[i].status = "Closed";
+      }
+    }
+      
+    localStorage.setItem('issues', JSON.stringify(issues));
+    
+    fetchIssues();
+  }
+
+  <a href="#" class="btn btn-danger" onclick="deleteIssue(\''+id+'\')">Delete</a>
+
+  function deleteIssue (id) {
+    var issues = JSON.parse(localStorage.getItem('issues'));
+    
+    for(var i = 0; i < issues.length; i++) {
+      if (issues[i].id == id) {
+        issues.splice(i, 1);
+      }
+    }
+    
+    localStorage.setItem('issues', JSON.stringify(issues));
+    
+    fetchIssues();
+  }
